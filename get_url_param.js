bd86b5825bc1b50592553f5b930c3244334ab0ed
eckhchri <ChristophEckhardt@web.de>
2015-07-16 17:41:11 +0200

    add support for overwriting DS-Url and DS-Port with http parameter dsurl and dsport.

---
+++ get_url_param.js
@@ -0,0 +1,13 @@
+function get_url_param( name )
+{
+	name = name.replace(/[\[]/,"\\\[").replace(/[\]]/,"\\\]");
+
+	var regexS = "[\\?&]"+name+"=([^&#]*)";
+	var regex = new RegExp( regexS );
+	var results = regex.exec( window.location.href );
+
+	if ( results == null )
+		return "";
+	else
+		return results[1];
+}
