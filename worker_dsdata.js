95d2e7b665981c33dc98c028b81f048718f0c044
eckhchri <ChristophEckhardt@web.de>
2015-07-17 19:48:56 +0200

    DSDATA renewal via HTML workers

--- worker_dsdata.js
+++ worker_dsdata.js
@@ -2,10 +2,10 @@
 
 self.addEventListener('message', function(e) {
 
-
+	console.log("e: " , e);
 	importScripts('./receive_ds_data.js');
 
-	var aDsData 	= Receive_DS_data( "www.eckhchri.de" , 9000 , 2000 , "GETDSDATA");
+	var aDsData 	= Receive_DS_data( e.data.dsurl, e.data.dsport , e.data.timeout , e.data.receivemode);
 	console.log("DSdata complete array: " , aDsData);
 
 	self.postMessage(aDsData);
