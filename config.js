fbe21fbb9e3a495417d88d686e87dfb38cbb80ff
eckhchri <ChristophEckhardt@web.de>
2016-02-04 18:40:53 +0100

    create config.js file including all user relevant variables. Addtional add variables to define if an object should displayed or not

--- config.js
+++ config.js
@@ -3,3 +3,9 @@ var DsServerURL 	= "MY-URL.com";
 var DsPort			= 9000;
 var CRESTServerURL 	= "localhost";
 var CRESTPort		= 8080;
+
+//hide or unhide elements
+var	SHOWTRACKLIST 	=	true;	// [true|false]
+var	SHOWDSDATA 		=	true;	// [true|false]
+var	SHOWDRIVERDATA 	=	true;	// [true|false]
+var SHOWSETTINGS	=	true;	// [true|false]
