81b5102dee2f88ed60378ad186a377efe5a3b7ea
eckhchri <ChristophEckhardt@web.de>
2016-02-05 08:28:08 +0100

    Move variables from index.html to config.js

--- config.js
+++ config.js
@@ -9,3 +9,7 @@ var	SHOWTRACKLIST 	=	true;	// [true|false]
 var	SHOWDSDATA 		=	true;	// [true|false]
 var	SHOWDRIVERDATA 	=	true;	// [true|false]
 var SHOWSETTINGS	=	true;	// [true|false]
+
+//defines a delay for each worker to reduce CPU load
+var WORKERDELEAY_TRACKLIST	=	5000; 	// in ms
+var WORKERDELEAY_DRIVERLIST	=	200;    // in ms
