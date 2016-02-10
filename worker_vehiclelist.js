f4deb3802f986c353c72e520f59d6fa639f416a0
viper4gh <f4.viper@web.de>
2016-02-10 14:57:23 +0100

    Initial worker code

--- worker_vehiclelist.js
+++ worker_vehiclelist.js
@@ -1,1 +1,25 @@
+//Worker thread for receiving DS Vehiclelist
 
+self.addEventListener('message', function(e) {
+
+	console.log("e: " , e);
+
+	importScripts('./receive_ds_data.js');
+	importScripts('./pcars_driver.js');
+	importScripts('./pcars_vehicle.js');
+	importScripts('./class_reference_points.js');
+	importScripts('./sleep.js');
+
+	//delays this worker to reduce http requests and make application resposive
+	sleep(e.data.workerdelay);
+
+	var aVehicleList 	= Receive_DS_data( e.data.dsurl, e.data.dsport , e.data.timeout , e.data.receivemode);
+	console.log("Worker: DS Vehiclelist from Receive_DS_data(): " , aVehicleList);
+//	console.log("Worker: DS Vehiclelist from Receive_DS_data() JSON " , JSON.stringify(aTrackList));
+
+
+	// use JSON.stringify() to prevent problem with empty arrays in Firefox
+//	self.postMessage(JSON.stringify(aVehicleList));
+	self.postMessage(aVehicleList);
+
+}, false);
