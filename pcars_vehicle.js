e0c4db436e424d4ccf2e8c0be0108d24e6ec656a
viper4gh <f4.viper@web.de>
2016-02-10 14:25:59 +0100

    PCARSVEHICLE class added

--- pcars_vehicle.js
+++ pcars_vehicle.js
@@ -1,1 +1,8 @@
-//new object
+// CLASS of an pCars vehicle
+function PCARSVEHICLE(vehicleid, name, class)
+{
+        // vars
+        this.name  	=       name;
+        this.vehicleid  	=       trackid;
+        this.class  	=       class;
+}
