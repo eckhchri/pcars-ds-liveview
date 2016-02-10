719a74110129f118cc5367c6facd91a66579b5ee
viper4gh <f4.viper@web.de>
2016-02-10 15:10:16 +0100

    variable class is not allowed, renamed to cls

--- pcars_vehicle.js
+++ pcars_vehicle.js
@@ -1,8 +1,8 @@
 // CLASS of an pCars vehicle
-function PCARSVEHICLE(vehicleid, name, class)
+function PCARSVEHICLE(vehicleid, name, cls)
 {
         // vars
         this.name  	=       name;
-        this.vehicleid  	=       trackid;
-        this.class  	=       class;
+        this.vehicleid  =       trackid;
+        this.cls  	=       cls;
 }
