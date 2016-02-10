062a83842b45048ecc8702371eb7e72adb1882e4
viper4gh <f4.viper@web.de>
2016-02-10 14:58:21 +0100

    Update worker_vehiclelist.js

--- worker_vehiclelist.js
+++ worker_vehiclelist.js
@@ -7,7 +7,6 @@ self.addEventListener('message', function(e) {
 	importScripts('./receive_ds_data.js');
 	importScripts('./pcars_driver.js');
 	importScripts('./pcars_vehicle.js');
-	importScripts('./class_reference_points.js');
 	importScripts('./sleep.js');
 
 	//delays this worker to reduce http requests and make application resposive
