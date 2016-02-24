6dd4a8affea34d6b87aa4523683d16699f6ea0f7
viper4gh <f4.viper@web.de>
2016-02-24 12:16:30 +0100

    log levels for console.log added

--- worker_tracklist.js
+++ worker_tracklist.js
@@ -6,6 +6,7 @@ self.addEventListener('message', function(e) {
 	importScripts('./pcars_driver.js');
 	importScripts('./pcars_track.js');
 	importScripts('./class_reference_points.js');
+	importScripts('./config.js');	//for console.log levels needed
 
 	//delays this worker to reduce http requests and make application resposive
 	//Syntax:  setTimeout(function,milliseconds,param1,param2,...)
@@ -17,7 +18,7 @@ self.addEventListener('message', function(e) {
 		//Workaround: needed to have a valid and full RefPoint hash in w_tracklist.addEventListener
 		a["aRefpoint"] = e.data.aRefpoint;
 		
-		//console.log("Worker: DS Tracklist from Receive_DS_data(): " , a);
+		//if(log >= 3){console.log("Worker: DS Tracklist from Receive_DS_data(): " , a);}
 
 		// return result to main thread after timeout
 		self.postMessage(a);
