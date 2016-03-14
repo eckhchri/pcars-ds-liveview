b60a637a02e49ad9aea284611e4b23ca577a8ddc
viper4gh <f4.viper@web.de>
2016-03-14 15:14:47 +0100

    updated

--- demo_data.js
+++ demo_data.js
@@ -1,44 +1,86 @@
 var demo = [
-                {//0
-                globals:{sensorLayer_UpdateDelta:3319, state:"Idle", name:"undefined", lobbyid:0, joinable:"false", max_member_count:0, now:1457944737, attributes:{TrackId:0, SessionState:"", SessionStage:"", GridSize:0, MaxPlayers:0}},
+		{//0
+                globals:{sensorLayer_UpdateDelta:3827, state:"Idle", name:"undefined", lobbyid:0, joinable:"false", max_member_count:0, now:1457964793, attributes:{TrackId:0, SessionState:"", SessionStage:"", GridSize:0, MaxPlayers:0}},
                 participants:[
-                        {Name:"NO_PARTICIPANT_TestData", IsPlayer:0, VehicleId:-886212684, RacePosition:0, CurrentLap:99, CurrentSector:0, LastLapTime:9999, FastestLapTime:9000, Speed:100, PositionX:271, PositionY:277, PositionZ:288, Orientation:0}
+                        {RefId:"undefined", Name:"NO_PARTICIPANT_TestData", IsPlayer:0, GridPosition:"3", VehicleId:-886212684, RacePosition:0, CurrentLap:99, CurrentSector:0, LastLapTime:9999, FastestLapTime:9000, Speed:100, PositionX:271, PositionY:277, PositionZ:288, Orientation:0}
                 ]
         },
-                {//1
-                globals:{sensorLayer_UpdateDelta:1337, state:"Idle", name:"undefined", lobbyid:0, joinable:"false", max_member_count:0, now:1457944738, attributes:{TrackId:0, SessionState:"", SessionStage:"", GridSize:0, MaxPlayers:0}},
+		{//1
+                globals:{sensorLayer_UpdateDelta:1175, state:"Idle", name:"undefined", lobbyid:0, joinable:"false", max_member_count:0, now:1457964794, attributes:{TrackId:0, SessionState:"", SessionStage:"", GridSize:0, MaxPlayers:0}},
                 participants:[
-                        {Name:"NO_PARTICIPANT_TestData", IsPlayer:0, VehicleId:-886212684, RacePosition:0, CurrentLap:99, CurrentSector:0, LastLapTime:9999, FastestLapTime:9000, Speed:100, PositionX:271, PositionY:277, PositionZ:288, Orientation:0}
+                        {RefId:"undefined", Name:"NO_PARTICIPANT_TestData", IsPlayer:0, GridPosition:"3", VehicleId:-886212684, RacePosition:0, CurrentLap:99, CurrentSector:0, LastLapTime:9999, FastestLapTime:9000, Speed:100, PositionX:271, PositionY:277, PositionZ:288, Orientation:0}
                 ]
         },
-                {//2
-                globals:{sensorLayer_UpdateDelta:968, state:"Idle", name:"undefined", lobbyid:0, joinable:"false", max_member_count:0, now:1457944739, attributes:{TrackId:0, SessionState:"", SessionStage:"", GridSize:0, MaxPlayers:0}},
+		{//2
+                globals:{sensorLayer_UpdateDelta:1263, state:"Idle", name:"undefined", lobbyid:0, joinable:"false", max_member_count:0, now:1457964796, attributes:{TrackId:0, SessionState:"", SessionStage:"", GridSize:0, MaxPlayers:0}},
                 participants:[
-                        {Name:"NO_PARTICIPANT_TestData", IsPlayer:0, VehicleId:-886212684, RacePosition:0, CurrentLap:99, CurrentSector:0, LastLapTime:9999, FastestLapTime:9000, Speed:100, PositionX:271, PositionY:277, PositionZ:288, Orientation:0}
+                        {RefId:"undefined", Name:"NO_PARTICIPANT_TestData", IsPlayer:0, GridPosition:"3", VehicleId:-886212684, RacePosition:0, CurrentLap:99, CurrentSector:0, LastLapTime:9999, FastestLapTime:9000, Speed:100, PositionX:271, PositionY:277, PositionZ:288, Orientation:0}
                 ]
         },
-                {//3
-                globals:{sensorLayer_UpdateDelta:751, state:"Idle", name:"undefined", lobbyid:0, joinable:"false", max_member_count:0, now:1457944740, attributes:{TrackId:0, SessionState:"", SessionStage:"", GridSize:0, MaxPlayers:0}},
+		{//3
+                globals:{sensorLayer_UpdateDelta:1261, state:"Idle", name:"undefined", lobbyid:0, joinable:"false", max_member_count:0, now:1457964797, attributes:{TrackId:0, SessionState:"", SessionStage:"", GridSize:0, MaxPlayers:0}},
                 participants:[
-                        {Name:"NO_PARTICIPANT_TestData", IsPlayer:0, VehicleId:-886212684, RacePosition:0, CurrentLap:99, CurrentSector:0, LastLapTime:9999, FastestLapTime:9000, Speed:100, PositionX:271, PositionY:277, PositionZ:288, Orientation:0}
+                        {RefId:"undefined", Name:"NO_PARTICIPANT_TestData", IsPlayer:0, GridPosition:"3", VehicleId:-886212684, RacePosition:0, CurrentLap:99, CurrentSector:0, LastLapTime:9999, FastestLapTime:9000, Speed:100, PositionX:271, PositionY:277, PositionZ:288, Orientation:0}
                 ]
         },
-                {//4
-                globals:{sensorLayer_UpdateDelta:799, state:"Idle", name:"undefined", lobbyid:0, joinable:"false", max_member_count:0, now:1457944741, attributes:{TrackId:0, SessionState:"", SessionStage:"", GridSize:0, MaxPlayers:0}},
+		{//4
+                globals:{sensorLayer_UpdateDelta:1296, state:"Idle", name:"undefined", lobbyid:0, joinable:"false", max_member_count:0, now:1457964798, attributes:{TrackId:0, SessionState:"", SessionStage:"", GridSize:0, MaxPlayers:0}},
                 participants:[
-                        {Name:"NO_PARTICIPANT_TestData", IsPlayer:0, VehicleId:-886212684, RacePosition:0, CurrentLap:99, CurrentSector:0, LastLapTime:9999, FastestLapTime:9000, Speed:100, PositionX:271, PositionY:277, PositionZ:288, Orientation:0}
+                        {RefId:"undefined", Name:"NO_PARTICIPANT_TestData", IsPlayer:0, GridPosition:"3", VehicleId:-886212684, RacePosition:0, CurrentLap:99, CurrentSector:0, LastLapTime:9999, FastestLapTime:9000, Speed:100, PositionX:271, PositionY:277, PositionZ:288, Orientation:0}
                 ]
         },
-                {//5
-                globals:{sensorLayer_UpdateDelta:977, state:"Idle", name:"undefined", lobbyid:0, joinable:"false", max_member_count:0, now:1457944742, attributes:{TrackId:0, SessionState:"", SessionStage:"", GridSize:0, MaxPlayers:0}},
+		{//5
+                globals:{sensorLayer_UpdateDelta:1314, state:"Idle", name:"undefined", lobbyid:0, joinable:"false", max_member_count:0, now:1457964800, attributes:{TrackId:0, SessionState:"", SessionStage:"", GridSize:0, MaxPlayers:0}},
                 participants:[
-                        {Name:"NO_PARTICIPANT_TestData", IsPlayer:0, VehicleId:-886212684, RacePosition:0, CurrentLap:99, CurrentSector:0, LastLapTime:9999, FastestLapTime:9000, Speed:100, PositionX:271, PositionY:277, PositionZ:288, Orientation:0}
+                        {RefId:"undefined", Name:"NO_PARTICIPANT_TestData", IsPlayer:0, GridPosition:"3", VehicleId:-886212684, RacePosition:0, CurrentLap:99, CurrentSector:0, LastLapTime:9999, FastestLapTime:9000, Speed:100, PositionX:271, PositionY:277, PositionZ:288, Orientation:0}
                 ]
         },
-                {//6
-                globals:{sensorLayer_UpdateDelta:1299, state:"Idle", name:"undefined", lobbyid:0, joinable:"false", max_member_count:0, now:1457944743, attributes:{TrackId:0, SessionState:"", SessionStage:"", GridSize:0, MaxPlayers:0}},
+		{//6
+                globals:{sensorLayer_UpdateDelta:1047, state:"Idle", name:"undefined", lobbyid:0, joinable:"false", max_member_count:0, now:1457964801, attributes:{TrackId:0, SessionState:"", SessionStage:"", GridSize:0, MaxPlayers:0}},
                 participants:[
-                        {Name:"NO_PARTICIPANT_TestData", IsPlayer:0, VehicleId:-886212684, RacePosition:0, CurrentLap:99, CurrentSector:0, LastLapTime:9999, FastestLapTime:9000, Speed:100, PositionX:271, PositionY:277, PositionZ:288, Orientation:0}
+                        {RefId:"undefined", Name:"NO_PARTICIPANT_TestData", IsPlayer:0, GridPosition:"3", VehicleId:-886212684, RacePosition:0, CurrentLap:99, CurrentSector:0, LastLapTime:9999, FastestLapTime:9000, Speed:100, PositionX:271, PositionY:277, PositionZ:288, Orientation:0}
+                ]
+        },
+		{//7
+                globals:{sensorLayer_UpdateDelta:1258, state:"Idle", name:"undefined", lobbyid:0, joinable:"false", max_member_count:0, now:1457964802, attributes:{TrackId:0, SessionState:"", SessionStage:"", GridSize:0, MaxPlayers:0}},
+                participants:[
+                        {RefId:"undefined", Name:"NO_PARTICIPANT_TestData", IsPlayer:0, GridPosition:"3", VehicleId:-886212684, RacePosition:0, CurrentLap:99, CurrentSector:0, LastLapTime:9999, FastestLapTime:9000, Speed:100, PositionX:271, PositionY:277, PositionZ:288, Orientation:0}
+                ]
+        },
+		{//8
+                globals:{sensorLayer_UpdateDelta:1296, state:"Idle", name:"undefined", lobbyid:0, joinable:"false", max_member_count:0, now:1457964803, attributes:{TrackId:0, SessionState:"", SessionStage:"", GridSize:0, MaxPlayers:0}},
+                participants:[
+                        {RefId:"undefined", Name:"NO_PARTICIPANT_TestData", IsPlayer:0, GridPosition:"3", VehicleId:-886212684, RacePosition:0, CurrentLap:99, CurrentSector:0, LastLapTime:9999, FastestLapTime:9000, Speed:100, PositionX:271, PositionY:277, PositionZ:288, Orientation:0}
+                ]
+        },
+		{//9
+                globals:{sensorLayer_UpdateDelta:2016, state:"Idle", name:"undefined", lobbyid:0, joinable:"false", max_member_count:0, now:1457964805, attributes:{TrackId:0, SessionState:"", SessionStage:"", GridSize:0, MaxPlayers:0}},
+                participants:[
+                        {RefId:"undefined", Name:"NO_PARTICIPANT_TestData", IsPlayer:0, GridPosition:"3", VehicleId:-886212684, RacePosition:0, CurrentLap:99, CurrentSector:0, LastLapTime:9999, FastestLapTime:9000, Speed:100, PositionX:271, PositionY:277, PositionZ:288, Orientation:0}
+                ]
+        },
+		{//10
+                globals:{sensorLayer_UpdateDelta:1601, state:"Idle", name:"undefined", lobbyid:0, joinable:"false", max_member_count:0, now:1457964807, attributes:{TrackId:0, SessionState:"", SessionStage:"", GridSize:0, MaxPlayers:0}},
+                participants:[
+                        {RefId:"undefined", Name:"NO_PARTICIPANT_TestData", IsPlayer:0, GridPosition:"3", VehicleId:-886212684, RacePosition:0, CurrentLap:99, CurrentSector:0, LastLapTime:9999, FastestLapTime:9000, Speed:100, PositionX:271, PositionY:277, PositionZ:288, Orientation:0}
+                ]
+        },
+		{//11
+                globals:{sensorLayer_UpdateDelta:1272, state:"Idle", name:"undefined", lobbyid:0, joinable:"false", max_member_count:0, now:1457964808, attributes:{TrackId:0, SessionState:"", SessionStage:"", GridSize:0, MaxPlayers:0}},
+                participants:[
+                        {RefId:"undefined", Name:"NO_PARTICIPANT_TestData", IsPlayer:0, GridPosition:"3", VehicleId:-886212684, RacePosition:0, CurrentLap:99, CurrentSector:0, LastLapTime:9999, FastestLapTime:9000, Speed:100, PositionX:271, PositionY:277, PositionZ:288, Orientation:0}
+                ]
+        },
+		{//12
+                globals:{sensorLayer_UpdateDelta:1285, state:"Idle", name:"undefined", lobbyid:0, joinable:"false", max_member_count:0, now:1457964809, attributes:{TrackId:0, SessionState:"", SessionStage:"", GridSize:0, MaxPlayers:0}},
+                participants:[
+                        {RefId:"undefined", Name:"NO_PARTICIPANT_TestData", IsPlayer:0, GridPosition:"3", VehicleId:-886212684, RacePosition:0, CurrentLap:99, CurrentSector:0, LastLapTime:9999, FastestLapTime:9000, Speed:100, PositionX:271, PositionY:277, PositionZ:288, Orientation:0}
+                ]
+        },
+		{//13
+                globals:{sensorLayer_UpdateDelta:1515, state:"Idle", name:"undefined", lobbyid:0, joinable:"false", max_member_count:0, now:1457964811, attributes:{TrackId:0, SessionState:"", SessionStage:"", GridSize:0, MaxPlayers:0}},
+                participants:[
+                        {RefId:"undefined", Name:"NO_PARTICIPANT_TestData", IsPlayer:0, GridPosition:"3", VehicleId:-886212684, RacePosition:0, CurrentLap:99, CurrentSector:0, LastLapTime:9999, FastestLapTime:9000, Speed:100, PositionX:271, PositionY:277, PositionZ:288, Orientation:0}
                 ]
         }
 ]
