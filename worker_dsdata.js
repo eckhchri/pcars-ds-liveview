6104ec196629ec220a041239ce33d6c01922b524
viper4gh <f4.viper@web.de>
2016-03-14 09:50:22 +0100

    script source demo_data.js added

--- worker_dsdata.js
+++ worker_dsdata.js
@@ -6,6 +6,7 @@ self.addEventListener('message', function(e) {
 	importScripts('./pcars_driver.js');
 	importScripts('./class_reference_points.js');
 	importScripts('./config.js');	//for console.log levels needed
+	importScripts('./demo_data.js');
 
 	//delays this worker to reduce http requests and make application resposive
 	//Syntax:  setTimeout(function,milliseconds,param1,param2,...)
