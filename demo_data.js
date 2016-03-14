df1a643e3e1519256054650357c94bb22f5b6102
viper4gh <f4.viper@web.de>
2016-03-14 09:48:10 +0100

    demo data for new Demo mode

---
+++ demo_data.js
@@ -0,0 +1,44 @@
+var demo = [
+                {//0
+                globals:{sensorLayer_UpdateDelta:3319, state:"Idle", name:"undefined", lobbyid:0, joinable:"false", max_member_count:0, now:1457944737, attributes:{TrackId:0, SessionState:"", SessionStage:"", GridSize:0, MaxPlayers:0}},
+                participants:[
+                        {Name:"NO_PARTICIPANT_TestData", IsPlayer:0, VehicleId:-886212684, RacePosition:0, CurrentLap:99, CurrentSector:0, LastLapTime:9999, FastestLapTime:9000, Speed:100, PositionX:271, PositionY:277, PositionZ:288, Orientation:0}
+                ]
+        },
+                {//1
+                globals:{sensorLayer_UpdateDelta:1337, state:"Idle", name:"undefined", lobbyid:0, joinable:"false", max_member_count:0, now:1457944738, attributes:{TrackId:0, SessionState:"", SessionStage:"", GridSize:0, MaxPlayers:0}},
+                participants:[
+                        {Name:"NO_PARTICIPANT_TestData", IsPlayer:0, VehicleId:-886212684, RacePosition:0, CurrentLap:99, CurrentSector:0, LastLapTime:9999, FastestLapTime:9000, Speed:100, PositionX:271, PositionY:277, PositionZ:288, Orientation:0}
+                ]
+        },
+                {//2
+                globals:{sensorLayer_UpdateDelta:968, state:"Idle", name:"undefined", lobbyid:0, joinable:"false", max_member_count:0, now:1457944739, attributes:{TrackId:0, SessionState:"", SessionStage:"", GridSize:0, MaxPlayers:0}},
+                participants:[
+                        {Name:"NO_PARTICIPANT_TestData", IsPlayer:0, VehicleId:-886212684, RacePosition:0, CurrentLap:99, CurrentSector:0, LastLapTime:9999, FastestLapTime:9000, Speed:100, PositionX:271, PositionY:277, PositionZ:288, Orientation:0}
+                ]
+        },
+                {//3
+                globals:{sensorLayer_UpdateDelta:751, state:"Idle", name:"undefined", lobbyid:0, joinable:"false", max_member_count:0, now:1457944740, attributes:{TrackId:0, SessionState:"", SessionStage:"", GridSize:0, MaxPlayers:0}},
+                participants:[
+                        {Name:"NO_PARTICIPANT_TestData", IsPlayer:0, VehicleId:-886212684, RacePosition:0, CurrentLap:99, CurrentSector:0, LastLapTime:9999, FastestLapTime:9000, Speed:100, PositionX:271, PositionY:277, PositionZ:288, Orientation:0}
+                ]
+        },
+                {//4
+                globals:{sensorLayer_UpdateDelta:799, state:"Idle", name:"undefined", lobbyid:0, joinable:"false", max_member_count:0, now:1457944741, attributes:{TrackId:0, SessionState:"", SessionStage:"", GridSize:0, MaxPlayers:0}},
+                participants:[
+                        {Name:"NO_PARTICIPANT_TestData", IsPlayer:0, VehicleId:-886212684, RacePosition:0, CurrentLap:99, CurrentSector:0, LastLapTime:9999, FastestLapTime:9000, Speed:100, PositionX:271, PositionY:277, PositionZ:288, Orientation:0}
+                ]
+        },
+                {//5
+                globals:{sensorLayer_UpdateDelta:977, state:"Idle", name:"undefined", lobbyid:0, joinable:"false", max_member_count:0, now:1457944742, attributes:{TrackId:0, SessionState:"", SessionStage:"", GridSize:0, MaxPlayers:0}},
+                participants:[
+                        {Name:"NO_PARTICIPANT_TestData", IsPlayer:0, VehicleId:-886212684, RacePosition:0, CurrentLap:99, CurrentSector:0, LastLapTime:9999, FastestLapTime:9000, Speed:100, PositionX:271, PositionY:277, PositionZ:288, Orientation:0}
+                ]
+        },
+                {//6
+                globals:{sensorLayer_UpdateDelta:1299, state:"Idle", name:"undefined", lobbyid:0, joinable:"false", max_member_count:0, now:1457944743, attributes:{TrackId:0, SessionState:"", SessionStage:"", GridSize:0, MaxPlayers:0}},
+                participants:[
+                        {Name:"NO_PARTICIPANT_TestData", IsPlayer:0, VehicleId:-886212684, RacePosition:0, CurrentLap:99, CurrentSector:0, LastLapTime:9999, FastestLapTime:9000, Speed:100, PositionX:271, PositionY:277, PositionZ:288, Orientation:0}
+                ]
+        }
+]
