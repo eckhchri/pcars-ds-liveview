26a6e88fd4e3ef34142810a405f91b962451c2ce
eckhchri <ChristophEckhardt@web.de>
2016-03-20 18:54:48 +0100

    fix CSS styles for cloring vehicles via drop down selection

--- css_classchanger.js
+++ css_classchanger.js
@@ -1,14 +1,16 @@
 // CLASS of an pCars Driver
 function CSSClassChanger ()
 {
-
-	this.aCSSClasses = new Array(); 	//Array of all handled CSS Clases
+	this.CurrentState	= "";
+	this.aCSSClasses 	= new Array(); 	//Array of all handled CSS Clases
+	// structure:
+	//		aCSSClasses['hideallsvgs']
+	//		aCSSClasses['colortop3']
+	//		...
 	
 	return this;
 
 }
-
-
 
 	// usage:
 	// add class:	
@@ -51,11 +53,9 @@ function CSSClassChanger ()
 	// hide svg objects during change of race session
 	function HideAllSvg()
 	{
-		
 		var CSSStyleHandle = setStyle(
 				    '.marker{ display: none; } \n',
 				    CSSStyleHandle );
-		
 		//remember object handle
 		this.aCSSClasses['hideallsvgs']	=	(CSSStyleHandle);
 		
@@ -65,14 +65,28 @@ function CSSClassChanger ()
 	function UnHideAllSvg(){
 		
 		if(this.aCSSClasses['hideallsvgs']) setStyle( '', this.aCSSClasses['hideallsvgs'] );
-		
 		delete this.aCSSClasses['hideallsvgs'];
 		
 		return 1;
 	}
-	
+	//color the first tree vehicles
+	function ColorTop3vehicles(){
+		
+		var CSScls;
+		var CSSstr = '';
+		CSSstr += 'circle.CSS_RacePos_1 {fill: gold; stroke-width: 3px }\n';
+		CSSstr += 'circle.CSS_RacePos_2 {fill: silver; stroke-width: 3px }\n';
+		CSSstr += 'circle.CSS_RacePos_3 {fill: #CD7F32; stroke-width: 3px }\n';
+		
+		this.aCSSClasses['colortop3'] = this.aCSSClasses['colortop3'] = setStyle( 
+										CSSstr
+										, CSScls );
+				
+		return 1;
+	}
 
 CSSClassChanger.prototype.setStyle=setStyle;
 CSSClassChanger.prototype.HideAllSvg=HideAllSvg;
 CSSClassChanger.prototype.UnHideAllSvg=UnHideAllSvg;
 CSSClassChanger.prototype.ClearAllCssClases=ClearAllCssClases;
+CSSClassChanger.prototype.ColorTop3vehicles=ColorTop3vehicles;
