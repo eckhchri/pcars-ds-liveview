beefea5ff717af97741272ec94b584ff7b370bfa
eckhchri <ChristophEckhardt@web.de>
2016-09-06 13:29:18 +0200

    Add first draft overlay for Replay and Statistics into toolbar. Delete redundant elements.

--- config.js
+++ config.js
@@ -17,6 +17,8 @@ var SHOWCARLIST 	=	true; 	// [true|false]
 var	SHOWDSDATA 		=	true;	// [true|false]
 var	SHOWDRIVERDATA 	=	true;	// [true|false]
 var SHOWSETTINGS	=	true;	// [true|false]
+var SHOWDSSTATISTIC =	false;	// [true|false]
+var SHOWDSRELAY		=	false;	// [true|false]
 
 //defines a delay for each worker to reduce CPU load
 var WORKERDELAY_TRACKLIST	=	5000; 	// in ms
