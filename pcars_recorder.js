9d8f99ffa3c96a3ac228482d1c08aad81fc49e87
eckhchri <ChristophEckhardt@web.de>
2016-10-20 16:01:44 +0200

    add a first version of import file to load saved racing data

--- pcars_recorder.js
+++ pcars_recorder.js
@@ -2,12 +2,15 @@
 function PCARSRECORDER(config)
 {
         // vars
-        this.maxRecordSize 		=	config.maxRecordSize;	// set max array size
-		this.DataVersion 		=	config.DataVersion;		// version of the data format etc
+        this.maxRecordSize 		=	config.maxRecordSize;			// set max array size
+		this.DataVersion 		=	config.DataVersion;				// version of the data format etc
+		this.FileContentPrefix	=	"___PCARSCompressedJSONC___";	// content prefix to identifiy the typ of compression for import decision
         
 		//TODO: Maybe allocate Array with maxRecordSize for optimize performance
-		this.CurrentDataSize	=	0;
-        this.data				=	[];   						// contains all replay data, each line includes one data record
+		this.CurrentDataSize	=	0;							// in kbyte
+		this.data				=	[];
+        this.data.racedata		=	[];   						// contains all replay data, each line includes one data record
+		this.data.common		=	[];   						// contains all replay data, each line includes one data record
 		this.aStatistics		=	[];							// array of severals statistics
 	
 	
@@ -32,41 +35,65 @@ function getDatasetSize() {
 function clearDataSet() {
 	// clear all saved data of the object
 	// TODO: or let array exist and only clear CurrentDataSize?? or this.data.lenght=0 ??
-	this.data = []; 
+	this.data.racedata 		=	[]; 
 	this.CurrentDataSize	=	0;
 	
 	return 1;
 }
 
-function loadData (path){
-	// SOURCE could be:  URL, LOCAL, HASH
-	//this.data = { lap1: {driver1: {posx: 21122; posy: 82766;}}}
-	
-	//TODO: if URL/LOCAL check data format version from
-	
-	// first clear all existing data  this.DataVersion
-	this.clearDataSet();
+function exportDataCompressed(filename){
+
+	// do not export empty array
+	if (this.data.racedata.length >= 1){
+		
+		//TODO: Change structure to this.data.racedata and add Information like 	ExportFormatVersion=1.0				
+		SaveAsFile(
+				//Compress a JSON object as a Gzipped string after compress it using JSONC +  JSON to Array
+				this.FileContentPrefix + JSONC.pack(JSON.stringify(this.data), true ),				
+				filename,
+				"text/plain;charset=utf-8");	
+	}else{
+		alert("Data Array empty. No Export possible")
+	}
 	
 	return 1;
 }
 
-function addDataset(racedata) {
+function importData(compressedData){
+	// SOURCE could be:  URL, LOCAL, HASH
+	//this.data = { lap1: {driver1: {posx: 21122; posy: 82766;}}}
+	//TODO: if URL/LOCAL check data format version from
+	//TODO:  //$.map( JSONC.pack( JSON.stringify(this.data), true ) , function(el) { return el }),	
+	
+	//Test data
+	compressedData 		= this.FileContentPrefix + JSONC.pack(JSON.stringify(this.data), true );
+	var isCompressed 	= false;
+	
+	// first clear all existing data  this.DataVersion
+	this.clearDataSet();
+	
+	
+	//if (compressed)
+	
+	
+	return 1;
+}
+
+
+
+function addDataset(dataset) {
 	// TODO:  define Format
-	if(log >= 3){console.log("---+++ PCARSRECORDER data added:   ", racedata);}
+	if(log >= 3){console.log("---+++ PCARSRECORDER data added:   ", dataset);}
 
 	// TODO: really needed or could be deletd because of performance issues?
 //	if ( this.data.length > this.maxDatasetSize ){
 		// max size reached, do not add addtional entires
 //		return 0;
 //	}
-	//this.data.raw.push({driver1, driver2, driver3});
-	
-	
-    this.data.push(racedata);
+			
+    this.data.racedata.push(dataset);
 	this.CurrentDataSize++;
-	
-	
-	
+		
 	return 1;
 }
 
@@ -114,6 +141,7 @@ function memorySizeOf(obj) {
 PCARSRECORDER.prototype.getStatistics	=	getStatistics;
 PCARSRECORDER.prototype.getDatasetSize	=	getDatasetSize;
 PCARSRECORDER.prototype.clearDataSet	=	clearDataSet;
-PCARSRECORDER.prototype.loadData		=	loadData;
+PCARSRECORDER.prototype.importData		=	importData;
 PCARSRECORDER.prototype.addDataset		=	addDataset;
 PCARSRECORDER.prototype.memorySizeOf	=	memorySizeOf;
+PCARSRECORDER.prototype.exportDataCompressed	=	exportDataCompressed;
