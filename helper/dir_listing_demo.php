5db3c9e9d7b1b54c812f17180a349315c6f54051
eckhchri <ChristophEckhardt@web.de>
2016-12-20 15:22:30 +0100

    add an popup to load demo files from URL with dynamic listing.

---
+++ helper/dir_listing_demo.php
@@ -0,0 +1,25 @@
+<?php
+
+$dir = "../data/demo/";
+
+if(is_dir($dir)){
+
+    if($dh = opendir($dir)){
+    	
+    	$a = array();
+        while(($file = readdir($dh)) != false){
+
+            if($file == "." or $file == ".." or strpos($file, 'zip') === false){
+			// skip listing for this files
+            } else {
+            	array_push($a, $file);	
+                //echo $file."<br />";
+                //echo json_encode($file);
+            }
+        }
+        
+        echo json_encode($a);
+    }
+}
+
+?>
