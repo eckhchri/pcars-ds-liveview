32edb88cfcc9504c47ff4fba72e8567f2711821f
eckhchri <ChristophEckhardt@web.de>
2017-01-03 10:46:42 +0100

    Change default demo data to Slightly Mad Studios Ltd headquarter in London with a extra small loop of one entry.

--- config.js
+++ config.js
@@ -54,15 +54,9 @@ aRecConfig['DataVersion']	=	1;			//in the case if data format will changed and r
 //demo initial data, currently that must not be empty
 var demo = [
                 {//0
-                    globals:{sensorLayer_UpdateDelta:886, state:"Running", name:"ViperDS", lobbyid:109775242963699993, joinable:"true", max_member_count:3, now:1459966148, attributes:{TrackId:1768660198, SessionState:"Lobby", SessionStage:"Practice1", GridSize:10, MaxPlayers:3, SessionTimeDuration:0, SessionTimeElapsed:0}},
+                    globals:{sensorLayer_UpdateDelta:886, state:"Running", name:"ViperDS", lobbyid:109775242963699993, joinable:"true", max_member_count:3, now:1459966148, attributes:{TrackId:9999999999, SessionState:"Lobby", SessionStage:"Practice1", GridSize:10, MaxPlayers:3, SessionTimeDuration:0, SessionTimeElapsed:0}},
                     participants:[
-                            {RefId:9234567, Name:"NO_PARTICIPANT_TestData", IsPlayer:0, GridPosition:3, VehicleId:"-886212684", RacePosition:0, CurrentLap:99, CurrentSector:0, LastLapTime:9999, FastestLapTime:9000, State:"StateTest", Speed:100, PosX:271, PosY:277, PosZ:288, Orientation:0}
-                    ]
-            },
-               {//1
-                    globals:{sensorLayer_UpdateDelta:662, state:"Running", name:"ViperDS", lobbyid:109775242963699993, joinable:"true", max_member_count:3, now:1459966149, attributes:{TrackId:1768660198, SessionState:"Lobby", SessionStage:"Practice1", GridSize:10, MaxPlayers:3, SessionTimeDuration:0, SessionTimeElapsed:0}},
-                    participants:[
-                            {RefId:9234567, Name:"NO_PARTICIPANT_TestData", IsPlayer:0, GridPosition:3, VehicleId:"Caterham Seven Classic", RacePosition:0, CurrentLap:99, CurrentSector:0, LastLapTime:9999, FastestLapTime:9000, State:"StateTest", Speed:100, PosX:271, PosY:277, PosZ:288, Orientation:0}
+                            {RefId:9234567, Name:"Slightly Mad Studios Ltd", IsPlayer:0, GridPosition:3, VehicleId:"-886212684", RacePosition:0, CurrentLap:99, CurrentSector:0, LastLapTime:9999, FastestLapTime:9000, State:"StateTest", Speed:100, PosX:271, PosY:277, PosZ:288, Orientation:0}
                     ]
             }
            ];
