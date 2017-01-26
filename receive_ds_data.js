3ec38ff4fee4ca0b35bac48edbdfa6a9ff786f8d
viper4gh <f4.viper@web.de>
2017-01-26 09:03:30 +0100

    mCurrentLap in CREST modes added

--- receive_ds_data.js
+++ receive_ds_data.js
@@ -358,7 +358,7 @@ function Receive_DS_data (url,port,timeout,receivemode, aRefPointTMP){
 								0,												//LastLapTime - NA
 								0,												//Orientation - NA
 								0,												//Speed - NA
-								999999,											//CurrentLap
+								myArr.participants.mParticipantInfo[i].mCurrentLap,						//CurrentLap
 								2091910841
 							);
 				}
@@ -419,7 +419,7 @@ function Receive_DS_data (url,port,timeout,receivemode, aRefPointTMP){
 								0,												//LastLapTime - NA
 								0,												//Orientation - NA
 								0,												//Speed - NA
-								999999,											//CurrentLap
+								myArr.participants.mParticipantInfo[i].mCurrentLap,						//CurrentLap
 								2091910841
 							);
 				}
