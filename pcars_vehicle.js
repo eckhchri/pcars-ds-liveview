16d53a954068cbcddd7c53d1ee63b58c8b2cc3ce
eckhchri <ChristophEckhardt@web.de>
2017-03-19 12:22:17 +0100

    Rename attribute availbility to gamescope

--- pcars_vehicle.js
+++ pcars_vehicle.js
@@ -1,12 +1,12 @@
 // CLASS of an pCars vehicle
-function PCARSVEHICLE(vehicleid, name, cls, link, availability, comment)
+function PCARSVEHICLE(vehicleid, name, cls, link, gamescope, comment)
 {
         // vars
         this.name			=	name;
         this.vehicleid		=	vehicleid;
         this.cls			=	cls;
         this.link			=	link;			//eg. hyperlink to wiki artice of this car 
-        this.availability	=	availability;	//PCARS1 and/or PCARS2
+        this.gamescope		=	gamescope;	//PCARS1 and/or PCARS2
         this.comment		=	'';				//additional info for each car
                     
         return this;
