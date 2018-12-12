939412ecd98c5b88699cd1e1256503410f850867
eckhchri <ChristophEckhardt@web.de>
2018-12-12 17:12:20 +0100

    Update config-sample.js
    Increase version tag to 0.4.0

--- config-sample.js
+++ config-sample.js
@@ -1,7 +1,7 @@
 ///////////////////////////////////////////////
 //global varibales definitions
 ///////////////////////////////////////////////
-var SCRIPTVERSION	= "0.3.8";
+var SCRIPTVERSION	= "0.4.0";
 
 // Defines the default MAP Type
 var DEFAULT_MAP_TYPE = "google"; // [google|bing|osm]
