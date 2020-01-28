af2ebc4c2df09d5add82be74c30ae22af4da7ab2
eckhchri <ChristophEckhardt@web.de>
2020-01-28 21:17:23 +0100

    Update config-sample.js
    Increase version to 0.4.1

--- config-sample.js
+++ config-sample.js
@@ -1,7 +1,7 @@
 ///////////////////////////////////////////////
 //global varibales definitions
 ///////////////////////////////////////////////
-var SCRIPTVERSION	= "0.4.0";
+var SCRIPTVERSION	= "0.4.1";
 
 // Defines the default MAP Type
 var DEFAULT_MAP_TYPE = "google"; // [google|bing|osm|raw]
