9d0802c2b3242261408bc2fd7e501dc970a84a2c
eckhchri <ChristophEckhardt@web.de>
2020-06-07 20:53:38 +0200

    Update config-sample.js
    Increase version tag to 0.5.2

--- config-sample.js
+++ config-sample.js
@@ -1,7 +1,7 @@
 ///////////////////////////////////////////////
 //global varibales definitions
 ///////////////////////////////////////////////
-var SCRIPTVERSION	= "0.5.1";
+var SCRIPTVERSION	= "0.5.2";
 
 // Defines the default MAP Type
 var DEFAULT_MAP_TYPE = "google"; // [google|bing|osm|raw]
