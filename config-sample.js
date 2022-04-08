706631f9cdd828755196bf138838ff2da8d72a22
eckhchri <eckhchri@eckhchri.de>
2022-04-08 10:43:36 +0200

    Bump version to 0.5.5 because of AMS2 content updates (track and cars).

--- config-sample.js
+++ config-sample.js
@@ -1,7 +1,7 @@
 ///////////////////////////////////////////////
 //global varibales definitions
 ///////////////////////////////////////////////
-var SCRIPTVERSION	= "0.5.4";
+var SCRIPTVERSION	= "0.5.5";
 
 // Defines the default MAP Type
 var DEFAULT_MAP_TYPE = "google"; // [google|bing|osm|raw]
